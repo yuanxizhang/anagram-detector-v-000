987e38d326f4fc7e4b38d53ec33c8705f9113026
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 21:31:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+# Build the Anagram class 
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(word_list) 
+    sorted = @word.split("").sort.join
+    word_list.select {|w| w.split("").sort.join == sorted}
+  end 
+end 
+  
+listen = Anagram.new("listen")
+puts listen.match(%w(enlists google inlets banana))
