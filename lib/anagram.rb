87ec8323672500da28e4d790bd674c244b095c3f
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 21:19:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+# Build the Anagram class 
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(word_list) 
+    word_list.select {|w| w.split.sort.join == @word.split.join}
+  end
+  
